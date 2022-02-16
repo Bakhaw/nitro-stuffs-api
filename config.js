3a9da5ba32c35c1151eea2773f33a0de0502cda3
Hattou Faïssal <faissal.hattou@gmail.com>
2022-02-16 17:38:11 +0100

    fix "npm run dev" command

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 export default {
   PORT: process.env.PORT || 3001,
-  // mongoUrl: 'mongodb://bakhaw:bakhaw9999@ds237363.mlab.com:37363/calendar'
   LOCAL_MONGO_URL: 'mongodb://localhost:27017/nitro-stuffs',
 };
